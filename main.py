480b18726a39beebcaf6f7df8bf10ff0dbf8799a
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-05-26 19:43:32 +0800

    rewrite

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+
+class FileEntry:
+	def __init__(self, absPath, destPath, fileName, ext):
+		self.srcPath = absPath
+		self.destPath = destPath
+		self.fileName = fileName
+		self.ext = ext
+
+def scanFiles(output_dir_name, path) -> tuple[list[FileEntry], set[str]]:
+	extensions = set()
+	file_entries: list[FileEntry] = []
+	baseDir = os.path.abspath(path)
+
+	for root, _, files in os.walk(path):
+
+		root = os.path.abspath(root)
+		for name in files:
+			abspath = os.path.join(root, name)
+			fileName, ext = os.path.splitext(name)
+			ext_clean = ext[1:] if ext else 'no_extension'
+			destPath = os.path.join(baseDir, output_dir_name, ext_clean, name)
+			extensions.add(ext_clean)
+			file_entries.append(FileEntry(abspath, destPath, fileName, ext))
+
+	return file_entries, extensions
+
+def moveFile(file: FileEntry):
+	# auto rename if file exists
+
+	if file.srcPath == file.destPath:
+		return
+
+	res = os.path.exists(file.destPath)
+	if not res:
+		os.rename(file.srcPath, file.destPath)
+		return
+
+	counter = 1
+	destDirName = os.path.dirname(file.destPath)
+
+	while res:
+		nums = range(counter, counter+5)
+		counter += 5
+		for i in nums:
+			newFileName = f"{file.fileName} ({i}){file.ext}"
+			newDest = os.path.join(destDirName, f"{newFileName}")
+			if not os.path.exists(newDest):
+				os.rename(file.srcPath, newDest)
+				res = False
+				break
+
+def main():
+	folder_name = "PySort"
+	targetDir = "."
+	files, extensions = scanFiles(folder_name, targetDir) 
+
+	# create output dir
+	if os.path.exists(folder_name):
+		print("Path Exists")
+	else:
+		os.mkdir(folder_name)
+
+	# create sub folders inside output dir
+	for ext in extensions:
+		folder_path = os.path.join(folder_name, ext)
+		if not os.path.exists(folder_path):
+			os.mkdir(folder_path)
+		else:
+			print(f"{ext} folder already exists")
+
+	# move files
+	for file in files:
+		# check if file exists
+		# check for duplicates and automatically rename
+		moveFile(file)
+
+	# delete empty folders
+	for root, dirs, _ in os.walk(targetDir, topdown=False):
+		for d in dirs:
+			dir_path = os.path.join(root, d)
+			if not os.listdir(dir_path):
+				print(f"able to delete: {dir_path}")
+				os.rmdir(dir_path)
+
+	# delete empty PySort folder
+	if not os.listdir(folder_name):
+		os.rmdir(folder_name)
+
+main()
