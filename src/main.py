e2ab2ccc7e1b26a131d63f1f5a41e8f6b90bd685
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2025-05-28 22:49:22 +0800

    unsort logic rewrite

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ class FileEntry:
 		self.fileName = fileName
 		self.ext = ext
 
-def scanFiles(output_dir_name, path) -> tuple[list[FileEntry], set[str]]:
+def scanFiles(path:str, outputDirName="PySort", unsortMode=False) -> tuple[list[FileEntry], set[str]]:
 	extensions = set()
 	file_entries: list[FileEntry] = []
 	baseDir = os.path.abspath(path)
@@ -19,7 +19,13 @@ def scanFiles(output_dir_name, path) -> tuple[list[FileEntry], set[str]]:
 			abspath = os.path.join(root, name)
 			fileName, ext = os.path.splitext(name)
 			ext_clean = ext[1:] if ext else 'no_extension'
-			destPath = os.path.join(baseDir, output_dir_name, ext_clean, name)
+			destPath = None
+
+			if unsortMode:
+				destPath = os.path.join(baseDir, name)
+			else:
+				destPath = os.path.join(baseDir, outputDirName, ext_clean, name)
+
 			extensions.add(ext_clean)
 			file_entries.append(FileEntry(abspath, destPath, fileName, ext))
 
@@ -51,33 +57,21 @@ def moveFile(file: FileEntry):
 				break
 		counter += 5
 
-def main():
-	folder_name = "PySort"
-	targetDir = input("Enter directory path to be sorted: ")
-
-	if not os.path.exists(targetDir):
-		print("\nProvided path doesn't exists.")
-		return
-
-	prompt = input(f"\nAre you sure you want to sort this directory?\n{os.path.abspath(targetDir)} (y/N): ")
-	if prompt[0].lower() != "y":
-		print("\nOperation cancelled")
-		return
-
-	print("Scanning files 🔎...")
+def sortFiles(targetDir: str, outputFolderName: str):
+	print("\nScanning files 🔎...")
 
 	# scan files recursively
-	files, extensions = scanFiles(folder_name, targetDir) 
+	files, extensions = scanFiles(targetDir) 
 
-	print(f"\nTotal files seen 🔎: {len(files)}")
+	print(f"Total files seen 🔎: {len(files)}\n")
 
 	# create output dir
-	if not os.path.exists(folder_name):
-		os.mkdir(folder_name)
+	if not os.path.exists(outputFolderName):
+		os.mkdir(outputFolderName)
 
 	# create sub folders inside output dir
 	for ext in extensions:
-		folder_path = os.path.join(folder_name, ext)
+		folder_path = os.path.join(outputFolderName, ext)
 		if not os.path.exists(folder_path):
 			os.mkdir(folder_path)
 
@@ -96,12 +90,54 @@ def main():
 		for d in dirs:
 			dir_path = os.path.join(root, d)
 			if not os.listdir(dir_path):
-				print(f"able to delete: {dir_path}")
 				os.rmdir(dir_path)
 
 	# delete empty PySort folder
-	if not os.listdir(folder_name):
-		os.rmdir(folder_name)
+	if not os.listdir(outputFolderName):
+		os.rmdir(outputFolderName)
 
 	print("\nOperation complete! ✅")
+
+def unsortFiles(targetDir: str):
+	print("\nScanning files 🔎...")
+
+	# scan files recursively
+	files, extensions = scanFiles(targetDir, unsortMode=True) 
+
+	print(f"Total files seen 🔎: {len(files)}\n")
+	print("Moving files, please wait 📂📂📂...")
+
+	# move files
+	for file in files:
+		# check if file exists
+		# check for duplicates and automatically rename
+		moveFile(file)
+
+	print("Cleanup 🧹🧹🧹...")
+
+	# delete empty folders
+	for root, dirs, _ in os.walk(targetDir, topdown=False):
+		for d in dirs:
+			dir_path = os.path.join(root, d)
+			if not os.listdir(dir_path):
+				os.rmdir(dir_path)
+
+	print("\nOperation complete! ✅")
+
+def main():
+	folder_name = "PySort"
+	targetDir = input("Enter directory path to be sorted: ")
+
+	if not os.path.exists(targetDir):
+		print("\nProvided path doesn't exists.")
+		return
+
+	prompt = input(f"\nAre you sure you want to sort this directory?\n{os.path.abspath(targetDir)} (y/N): ")
+	if prompt[0].lower() != "y":
+		print("\nOperation cancelled")
+		return
+	
+	sortFiles(targetDir, folder_name)
+	# unsortFiles(targetDir)
+
 main()
